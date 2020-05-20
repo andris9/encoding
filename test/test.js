5122939b5796c3e597a2671215d01f26f978a639
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-05-20 16:07:07 -0700

    Use safer-buffer for safer buffer usages

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Buffer = require('safer-buffer').Buffer;
+
 var Iconv = require('../lib/iconv-loader');
 var encoding = require('../lib/encoding');
 
@@ -12,13 +14,13 @@ exports['General tests'] = {
 
     'From UTF-8 to Latin_1 with Iconv': function (test) {
         var input = 'ÕÄÖÜ',
-            expected = new Buffer([0xd5, 0xc4, 0xd6, 0xdc]);
+            expected = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc]);
         test.deepEqual(encoding.convert(input, 'latin1'), expected);
         test.done();
     },
 
     'From Latin_1 to UTF-8 with Iconv': function (test) {
-        var input = new Buffer([0xd5, 0xc4, 0xd6, 0xdc]),
+        var input = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc]),
             expected = 'ÕÄÖÜ';
         test.deepEqual(encoding.convert(input, 'utf-8', 'latin1').toString(), expected);
         test.done();
@@ -26,34 +28,34 @@ exports['General tests'] = {
 
     'From UTF-8 to UTF-8 with Iconv': function (test) {
         var input = 'ÕÄÖÜ',
-            expected = new Buffer('ÕÄÖÜ');
+            expected = Buffer.from('ÕÄÖÜ');
         test.deepEqual(encoding.convert(input, 'utf-8', 'utf-8'), expected);
         test.done();
     },
 
     'From Latin_13 to Latin_15 with Iconv': function (test) {
-        var input = new Buffer([0xd5, 0xc4, 0xd6, 0xdc, 0xd0]),
-            expected = new Buffer([0xd5, 0xc4, 0xd6, 0xdc, 0xA6]);
+        var input = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc, 0xd0]),
+            expected = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc, 0xA6]);
         test.deepEqual(encoding.convert(input, 'latin_15', 'latin13'), expected);
         test.done();
     },
 
     'From ISO-2022-JP to UTF-8 with Iconv': function (test) {
-        var input = new Buffer('GyRCM1g5OzU7PVEwdzgmPSQ4IUYkMnFKczlwGyhC', 'base64'),
-            expected = new Buffer('5a2m5qCh5oqA6KGT5ZOh56CU5L+u5qSc6KiO5Lya5aCx5ZGK', 'base64');
+        var input = Buffer.from('GyRCM1g5OzU7PVEwdzgmPSQ4IUYkMnFKczlwGyhC', 'base64'),
+            expected = Buffer.from('5a2m5qCh5oqA6KGT5ZOh56CU5L+u5qSc6KiO5Lya5aCx5ZGK', 'base64');
         test.deepEqual(encoding.convert(input, 'utf-8', 'ISO-2022-JP'), expected);
         test.done();
     },
 
     'From UTF-8 to Latin_1 with iconv-lite': function (test) {
         var input = 'ÕÄÖÜ',
-            expected = new Buffer([0xd5, 0xc4, 0xd6, 0xdc]);
+            expected = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc]);
         test.deepEqual(encoding.convert(input, 'latin1', false, true), expected);
         test.done();
     },
 
     'From Latin_1 to UTF-8 with iconv-lite': function (test) {
-        var input = new Buffer([0xd5, 0xc4, 0xd6, 0xdc]),
+        var input = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc]),
             expected = 'ÕÄÖÜ';
         test.deepEqual(encoding.convert(input, 'utf-8', 'latin1', true).toString(), expected);
         test.done();
@@ -61,14 +63,14 @@ exports['General tests'] = {
 
     'From UTF-8 to UTF-8 with iconv-lite': function (test) {
         var input = 'ÕÄÖÜ',
-            expected = new Buffer('ÕÄÖÜ');
+            expected = Buffer.from('ÕÄÖÜ');
         test.deepEqual(encoding.convert(input, 'utf-8', 'utf-8', true), expected);
         test.done();
     },
 
     'From Latin_13 to Latin_15 with iconv-lite': function (test) {
-        var input = new Buffer([0xd5, 0xc4, 0xd6, 0xdc, 0xd0]),
-            expected = new Buffer([0xd5, 0xc4, 0xd6, 0xdc, 0xA6]);
+        var input = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc, 0xd0]),
+            expected = Buffer.from([0xd5, 0xc4, 0xd6, 0xdc, 0xA6]);
         test.deepEqual(encoding.convert(input, 'latin_15', 'latin13', true), expected);
         test.done();
     }
